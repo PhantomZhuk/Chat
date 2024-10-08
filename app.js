cc3cdd73647079feda37720bbcc3413f2580439d
Maksym Zhuk <bitmaksum550@gmail.com>
2024-10-08 20:55:44 +0300

    add authorisation

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 require('dotenv').config();
-
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(`public`));
+
 
 app.get('/chats', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -16,6 +21,63 @@ app.get('/chats', (req, res) => {
 app.get('/auth', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', `auth`, 'index.html'));
 });
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log(`MongoDB connected`);
+    })
+
+const userSchema = new mongoose.Schema({
+    login: String,
+    password: String
+})
+
+const User = mongoose.model("User", userSchema);
+
+app.post(`/auth/createUser`, (req, res) => {
+    const { login, password } = req.body;
+    if (login && password) {
+        const newUser = new User({ login, password });
+        newUser.save();
+        res.sendStatus(201);
+    }else {
+        res.sendStatus(500);
+    }
+})
+
+app.post('/auth/login', async (req, res) => {
+    const { login, password } = req.body;
+    if (!login || !password) {
+        return res.status(400).send('Missing login or password');
+    }
+
+    try {
+        const user = await User.findOne({ login, password });
+
+        if (user) {
+            res.sendStatus(200); 
+        } else {
+            res.sendStatus(401);
+        }
+
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
+
+app.get('/allUsers', async (req, res) => {
+    try {
+        const users = await User.find();
+        
+        res.send(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 let connectionUsers = 0;
 
@@ -38,6 +100,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('listening on 3000');
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
 });
--- public/auth/main.js
+++ public/auth/main.js
@@ -15,4 +15,68 @@ $(document).ready(() => {
         $(`#openRegisterBtn`).addClass('active');
         $(`#openRegisterBtn`).removeClass('notActive');
     })
+
+    let userToken = $.cookie('userToken') || '';
+
+    $('#registerBtn').on('click', () => {
+        const login = $('#registerInput').val();
+        const password = $('#registerPasswordInput').val();
+
+        const loginRegex = /[a-zA-Z]{3,}/;
+        const passwordRegex = /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}/;
+
+        if (loginRegex.test(login)) {
+            if (passwordRegex.test(password)) {
+                axios.get('/allUsers')
+                    .then((res) => {
+                        const existingUser = res.data.find(el => el.login === login);
+
+                        if (!existingUser) {
+                            axios.post('/auth/createUser', { login, password })
+                                .then(() => {
+                                    console.log('User created');
+                                    axios.get('/allUsers')
+                                        .then((res) => {
+                                            const newUser = res.data.find(el => el.login === login && el.password === password);
+
+                                            if (newUser) {
+                                                userToken = $.cookie('userToken', newUser._id, { path: '/' });
+                                                window.location.href = '/chats';
+                                                $('#registerInput').val(``);
+                                                $('#registerPasswordInput').val(``);
+                                            } else {
+                                                alert('User not found');
+                                            }
+                                        })
+                                        .catch((error) => {
+                                            console.error('Error getting users:', error);
+                                        });
+                                })
+                                .catch((error) => {
+                                    console.error('Error creating user:', error);
+                                });
+                        } else {
+                            console.log('User already exists');
+                        }
+                    })
+                    .catch((error) => {
+                        console.error('Error getting users:', error);
+                    });
+            } else {
+                console.log('Incorrect password');
+            }
+        } else {
+            console.log('Incorrect login');
+        }
+    });
+
+    $('#loginBtn').on('click', () => {
+        axios.post(`/auth/login`, { login: $('#loginInput').val(), password: $('#passwordInput').val() })
+            .then(() => {
+                console.log('User logged in');
+                window.location.href = '/chats';
+                $('#registerInput').val(``);
+                $('#registerPasswordInput').val(``);
+            })
+    })
 })
