905ba43de1ed069587304f13b700ec4f5b1126aa
Maksym Zhuk <bitmaksum550@gmail.com>
2024-10-13 20:53:16 +0300

    add an opening and change the menu

--- public/main.js
+++ public/main.js
@@ -59,10 +59,83 @@ function getUserNameById(id, callback) {
 }
 
 function appendMessage(type, userName, message) {
-    $('.messageContainer').append( `
+    $('.messageContainer').append(`
         <div class="${type}">
             <div class="nameUser">${userName}</div>
             <div class="message">${message}</div>
         </div>
     `);
 }
+
+$(`#MenuBtn`).click(() => { 
+    if ($(`#MenuBtn`).hasClass(`fa-bars`)) {
+        $(`.mainChatContainer`).css('margin-top', '10px');
+        $(`header`).css('width', '300px');
+        $(`.mainChat`).css('background-color', '#2d5362');
+        $(`.mainChat`).css('justify-content', 'flex-start');
+        $(`.mainChatIcon`).css('margin', '10px');
+        $(`.mainChatName`).css('display', 'flex');
+        $(`.addChatBtn`).css('width', '90%');
+        $(`.menuBtnContainer`).css(`justify-content`, `space-between`);
+        $(`.menuBtnContainer`).css(`padding`, `0 20px`);
+        $(`.accountContainer`).css(`display`, `flex`);
+        $(`.settingBtn`).css(`justify-content`, `flex-start`);
+        $(`.settingBtn`).css(`padding`, `0 20px`);
+        $(`#MenuBtn`).addClass(`fa-xmark`);
+        $(`#MenuBtn`).removeClass(`fa-bars`);
+        $(`#searchInput`).css(`display`, `flex`);
+        $(`.searchContainer`).css(`justify-content`, `space-between`);
+        $(`#searchBtn`).css(`width`, `40px`);
+        $(`#searchBtn`).css(`height`, `40px`);
+        $(`.settingsText`).css(`display`, `flex`);
+    }else if ($(`#MenuBtn`).hasClass(`fa-xmark`)) {
+        $(`.mainChatContainer`).css('margin-top', '0');
+        $(`header`).css('width', '100px');
+        $(`.mainChat`).css('background-color', '#264653');
+        $(`.mainChat`).css('justify-content', 'center');
+        $(`.mainChatIcon`).css('margin', '0');
+        $(`.mainChatName`).css('display', 'none');
+        $(`.addChatBtn`).css('width', '65px');
+        $(`.menuBtnContainer`).css(`justify-content`, `center`);
+        $(`.menuBtnContainer`).css(`padding`, `0`);
+        $(`.accountContainer`).css(`display`, `none`);
+        $(`.settingBtn`).css(`justify-content`, `center`);
+        $(`.settingBtn`).css(`padding`, `0`);
+        $(`#MenuBtn`).addClass(`fa-bars`);
+        $(`#MenuBtn`).removeClass(`fa-xmark`);
+        $(`#searchInput`).css(`display`, `none`);
+        $(`.searchContainer`).css(`justify-content`, `center`);
+        $(`#searchBtn`).css(`width`, `65px`);
+        $(`#searchBtn`).css(`height`, `65px`);
+        $(`#searchBtn`).addClass(`openMenu`);
+        $(`#searchBtn`).removeClass(`search`);
+        $(`.settingsText`).css(`display`, `none`);
+    }
+});
+
+$(`#searchBtn`).click(() => {
+    if($(`#searchBtn`).hasClass(`openMenu`)) {
+        $(`.mainChatContainer`).css('margin-top', '10px');
+        $(`header`).css('width', '300px');
+        $(`.mainChat`).css('background-color', '#2d5362');
+        $(`.mainChat`).css('justify-content', 'flex-start');
+        $(`.mainChatIcon`).css('margin', '10px');
+        $(`.mainChatName`).css('display', 'flex');
+        $(`.addChatBtn`).css('width', '90%');
+        $(`.menuBtnContainer`).css(`justify-content`, `space-between`);
+        $(`.menuBtnContainer`).css(`padding`, `0 20px`);
+        $(`.accountContainer`).css(`display`, `flex`);
+        $(`.settingBtn`).css(`justify-content`, `flex-start`);
+        $(`.settingBtn`).css(`padding`, `0 20px`);
+        $(`#MenuBtn`).addClass(`fa-xmark`);
+        $(`#MenuBtn`).removeClass(`fa-bars`);
+        $(`#searchInput`).css(`display`, `flex`);
+        $(`.searchContainer`).css(`justify-content`, `space-between`);
+        $(`#searchBtn`).css(`width`, `40px`);
+        $(`#searchBtn`).css(`height`, `40px`);
+        $(`#searchBtn`).removeClass(`openMenu`);
+        $(`#searchBtn`).addClass(`search`);
+    }else if($(`#MenuBtn`).hasClass(`fa-xmark`)) {
+        console.log(`Search button clicked`);
+    }
+});
